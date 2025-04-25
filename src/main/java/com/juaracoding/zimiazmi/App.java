42a3e87a8df1d5610179002fd67104ed8be5eed4
zimiazmi <zimiazmi04@gmail.com>
2025-04-25 23:31:11 +0700

    tambah pom dan direktori

---
+++ src/main/java/com/juaracoding/zimiazmi/App.java
@@ -0,0 +1,13 @@
+package com.juaracoding.zimiazmi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
