20aae8d51f771e8610b607daf0eedf3f8ad6d627
zimiazmi <zimiazmi04@gmail.com>
2025-04-26 10:10:50 +0700

    tambah import hooks

--- src/test/java/com/juaracoding/cucumber/hooks/Hooks.java
+++ src/test/java/com/juaracoding/cucumber/hooks/Hooks.java
@@ -1,6 +1,20 @@
 package com.juaracoding.cucumber.hooks;
 
+import com.juaracoding.cucumber.utils.Browser;
+import com.juaracoding.cucumber.utils.DriverSingleton;
+import com.juaracoding.cucumber.utils.Screenshotutil;
+import com.relevantcode.extentreports.ExtentReports;
+import com.relevantcode.extentreports.ExtentTest;
+import com.relevantcode.extentreports.LogStatus;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+import static com.juaracoding.cucumber.utils.DateUtil.*;
+
 public class Hooks {
+
+    
     // This class is used to define hooks for Cucumber tests
     // Currently, it does not contain any methods or fields
     // You can add setup and teardown methods here if needed
