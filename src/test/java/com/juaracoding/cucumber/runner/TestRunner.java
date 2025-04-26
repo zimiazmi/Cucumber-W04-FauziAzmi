486a98d3a7a2d4783ab8bb12ab909a5600f59f27
zimiazmi <zimiazmi04@gmail.com>
2025-04-27 01:22:54 +0700

    just check

--- src/test/java/com/juaracoding/cucumber/runner/TestRunner.java
+++ src/test/java/com/juaracoding/cucumber/runner/TestRunner.java
@@ -3,11 +3,12 @@ package com.juaracoding.cucumber.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+@RunWith(Cucumber.class)
 @CucumberOptions
         (features = {"src/test/resources/features"},
         glue = {"com/juaracoding/cucumber/stepdefinitions", "com/juaracoding/cucumber/hooks"},
         plugin = {"pretty","html:target/cucumber-report.html","json:target/cucumber.json"})
 
-@Test
+
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
